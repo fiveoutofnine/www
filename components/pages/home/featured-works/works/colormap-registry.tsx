cf94f65629c21ff2144e9a5f03158651f279dc6a
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-04-30 10:48:13 -0400

    feat: show tooltip on hover over colormap

--- components/pages/home/featured-works/works/colormap-registry.tsx
+++ components/pages/home/featured-works/works/colormap-registry.tsx
@@ -1,5 +1,6 @@
-import { type FC, useState } from 'react';
+import { type FC, type PointerEvent, useCallback, useState } from 'react';
 
+import { TooltipWithBounds, useTooltip, useTooltipInPortal } from '@visx/tooltip';
 import clsx from 'clsx';
 import { LayoutGroup, motion } from 'framer-motion';
 import { ArrowLeft, ExternalLink, Github } from 'lucide-react';
@@ -55,6 +56,26 @@ const ColormapRegistryFeatureDetail: FC = () => {
     'bg-purple-9',
     'bg-pink-9',
   ];
+  const { containerRef, containerBounds } = useTooltipInPortal({
+    scroll: true,
+    detectBounds: true,
+  });
+
+  const {
+    showTooltip,
+    hideTooltip,
+    tooltipOpen,
+    tooltipLeft = 0,
+  } = useTooltip<string>({ tooltipOpen: true, tooltipLeft: 0, tooltipTop: 62 });
+
+  const handlePointerMove = useCallback(
+    (event: PointerEvent<HTMLDivElement>) => {
+      // Coordinates should be relative to the container
+      const containerX = ('clientX' in event ? event.clientX : 0) - containerBounds.left;
+      showTooltip({ tooltipLeft: containerX, tooltipTop: 62 });
+    },
+    [showTooltip, containerBounds],
+  );
 
   return (
     <div className="relative bg-gray-3">
@@ -76,26 +97,61 @@ const ColormapRegistryFeatureDetail: FC = () => {
                 transition={{ type: 'tween', duration: 0.15 }}
               />
               {selected === index ? (
-                <motion.div
-                  className={clsx('absolute left-0 top-0 h-full w-full', COLORMAPS[selected])}
-                  layoutId={`colormap-${index}`}
-                  transition={{ type: 'tween', duration: 0.15 }}
+                <div
+                  className="absolute left-0 top-0 h-full w-full"
+                  ref={containerRef}
+                  onPointerMove={handlePointerMove}
+                  onMouseLeave={hideTooltip}
                 >
                   <motion.div
-                    className="flex h-8 w-full items-center justify-between border-b border-gray-6 bg-gray-3 px-1 backdrop-blur-2xl"
-                    initial={{ height: 0, opacity: 0 }}
-                    animate={{ height: 32, opacity: 1 }}
-                    transition={{ type: 'spring', delay: 0.1, duration: 0.25 }}
+                    className={clsx('h-full w-full', COLORMAPS[selected])}
+                    layoutId={`colormap-${index}`}
+                    transition={{ type: 'tween', duration: 0.15 }}
                   >
-                    <IconButton size="sm" onClick={() => setSelected(undefined)}>
-                      <ArrowLeft />
-                    </IconButton>
-                    <span className="text-xs font-medium text-gray-12">Winter</span>
-                    <IconButton size="sm">
-                      <ExternalLink />
-                    </IconButton>
+                    <motion.div
+                      className="flex h-8 w-full items-center justify-between border-b border-gray-6 bg-gray-3 px-1"
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{ height: 32, opacity: 1 }}
+                      transition={{ type: 'spring', delay: 0.1, duration: 0.25 }}
+                    >
+                      <IconButton
+                        size="sm"
+                        onClick={() => {
+                          setSelected(undefined);
+                          hideTooltip();
+                        }}
+                      >
+                        <ArrowLeft />
+                      </IconButton>
+                      <span className="text-xs font-medium text-gray-12">Winter</span>
+                      <IconButton size="sm">
+                        <ExternalLink />
+                      </IconButton>
+                    </motion.div>
+                    {tooltipOpen ? (
+                      <>
+                        <hr
+                          className="absolute h-28 border border-gray-7"
+                          style={{ transform: `translateX(${tooltipLeft}px)` }}
+                        />
+                        <TooltipWithBounds
+                          key={Math.random()} // Needed for bounds to update correctly
+                          left={tooltipLeft}
+                          top={62}
+                          className="flex h-8 items-center justify-center rounded-lg border border-gray-6 bg-gray-4 px-2 font-mono text-xs font-medium"
+                          style={{
+                            top: 0,
+                            left: 0,
+                            position: 'absolute',
+                            pointerEvents: 'none',
+                          }}
+                        >
+                          #{tooltipLeft?.toFixed(0)}
+                        </TooltipWithBounds>
+                      </>
+                    ) : null}
                   </motion.div>
-                </motion.div>
+                </div>
               ) : null}
             </LayoutGroup>
           );
