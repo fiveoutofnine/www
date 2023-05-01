c00f9f12bf4315b6611c74d62f57c3dc4304a04e
5/9 <66016924+fiveoutofnine@users.noreply.github.com>
2023-05-01 15:50:42 +0200

    feat(v1) tx.cool feature (#6)
    * feat: init tx.cool feature
    
    * chore: update link
    
    * feat: chat header component
    
    * feat: send message input
    
    * fix: input accessibility
    
    * chore: run lint
    
    * feat: show messages with tx hashes
    
    * fix: render longer messages nicer
    
    * chore: add first message
    
    * feat: add toast
    
    * fix: toast styles and toasts flow
    
    * chore: some fixes and add txs
    
    * fix: typos
    
    * chore: remove unnecessary classes
    
    ---------
    
    Co-authored-by: e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>

--- components/pages/home/featured-works/index.tsx
+++ components/pages/home/featured-works/index.tsx
@@ -4,14 +4,17 @@ import BitTwiddlingFeature from './works/bit-twiddling';
 import ChessFeature from './works/chess';
 import ColormapRegistryFeature from './works/colormap-registry';
 import TwitterThreadFeature from './works/twitter-thread';
+import TxDotCoolFeature from './works/txdotcool';
 import TypingFeature from './works/typing';
 
 const FeaturedWorks: FC = () => {
   return (
     <div className="min-[960px]:grid-cols-6 min-[560px]:grid-cols-4 grid grid-cols-2 gap-4">
       <ChessFeature />
-      <ColormapRegistryFeature />
+      <TxDotCoolFeature />
       <TypingFeature />
+      <div className="min-[560px]:col-span-4 col-span-2 rounded-xl border border-gray-6 bg-gray-2" />
+      <ColormapRegistryFeature />
       <BitTwiddlingFeature />
       <TwitterThreadFeature />
     </div>
---
+++ components/ui/toaster/types.tsx
@@ -0,0 +1,29 @@
+import type { ComponentPropsWithoutRef } from 'react';
+
+import { toastDescriptionVariants, toastTitleVariants, toastVariants } from './styles';
+import * as ToastPrimitives from '@radix-ui/react-toast';
+import type { VariantProps } from 'class-variance-authority';
+
+/* Variant Props */
+export type ToastDescriptionVariantProps = VariantProps<typeof toastDescriptionVariants>;
+
+export type ToastTitleVariantProps = VariantProps<typeof toastTitleVariants>;
+
+export type ToastVariantProps = VariantProps<typeof toastVariants>;
+
+/* Component Props */
+export type ToastActionProps = ComponentPropsWithoutRef<typeof ToastPrimitives.Action> &
+  Pick<ToastProps, 'intent'>;
+
+export type ToastCloseProps = ComponentPropsWithoutRef<typeof ToastPrimitives.Close> &
+  Pick<ToastProps, 'intent'>;
+
+export type ToastDescriptionProps = ComponentPropsWithoutRef<typeof ToastPrimitives.Description> &
+  ToastDescriptionVariantProps;
+
+export type ToastProps = ComponentPropsWithoutRef<typeof ToastPrimitives.Root> & ToastVariantProps;
+
+export type ToastTitleProps = ComponentPropsWithoutRef<typeof ToastPrimitives.Title> &
+  ToastTitleVariantProps;
+
+export type ToastViewport = ComponentPropsWithoutRef<typeof ToastPrimitives.Viewport>;
