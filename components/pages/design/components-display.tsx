48e6e554b15f96acbfe71daf1099c4cae2f0ff2f
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-06-16 03:45:07 +0900

    fix: grid cols not applying

--- components/pages/design/components-display.tsx
+++ components/pages/design/components-display.tsx
@@ -1,44 +1,17 @@
-import { type FC, useMemo } from 'react';
+import type { FC } from 'react';
 
 import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-/* Props */
-type DesignComponentsDisplayProps = JSX.IntrinsicElements['div'] & {
-  columns?: (1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12)[];
-};
-
-/* Component */
-const DesignComponentsDisplay: FC<DesignComponentsDisplayProps> = ({
-  columns = [2, 4],
+const DesignComponentsDisplay: FC<JSX.IntrinsicElements['div']> = ({
+  className,
   children,
   ...rest
 }) => {
-  const columnClasses = useMemo(
-    () =>
-      columns
-        .splice(0, 5)
-        .map((column, index) => {
-          const columnClass = `grid-cols-${column}`;
-
-          return index === 4
-            ? `2xl:${columnClass}`
-            : index === 3
-            ? `xl:${columnClass}`
-            : index === 2
-            ? `lg:${columnClass}`
-            : index === 1
-            ? `md:${columnClass}`
-            : columnClass;
-        })
-        .join(' '),
-    [columns],
-  );
-
   return (
     <div
-      className={clsx(
-        'grid w-full gap-4 rounded-xl border border-gray-6 bg-gray-2 p-8',
-        columnClasses,
+      className={twMerge(
+        clsx('grid w-full gap-4 rounded-xl border border-gray-6 bg-gray-2 p-8', className),
       )}
       {...rest}
     >
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   darkMode: 'class',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
