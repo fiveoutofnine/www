56e7bfbcb793f5d936b70280b7513c4ba27aa883
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-06-17 01:32:48 +0900

    feat: add metadata

--- components/layouts/design.tsx
+++ components/layouts/design.tsx
@@ -1,6 +1,7 @@
 import type { FC, ReactNode } from 'react';
 
 import { MDXProvider } from '@mdx-js/react';
+import { NextSeo } from 'next-seo';
 
 import type { PageSlug } from '@/lib/types/site';
 
@@ -52,25 +53,36 @@ const DesignLayout: FC<DesignLayoutProps> = ({ name, selected, children }) => {
   };
 
   return (
-    <BaseLayout name={name} pageSlug="/design">
-      {/* Note: `pb-6` overrides `pb-4` on small devices. `<DesignNavBar />`
-      has a `mb-6` when displayed on small screens, so the ``margin''
-      above/below the article content is symmetrical. We do this instead of
-      `py-6` to correctly position `<DesignNavBar />`, as well as the article
-      content on small screens. This positioning issue is not present on larger
-      screens so we have a breakpoint to reset it. For similar reasons, the
-      `x` padding is set to 0 on small devices is set to 0. */}
-      <ContainerLayout className="relative flex max-w-[90rem] flex-col space-x-0 px-0 pb-6 pt-0 md:flex-row md:space-x-16">
-        <DesignNavBar selected={selected} />
-        <MDXProvider components={components}>
-          <article className="prose prose-gray max-w-none grow px-4 dark:prose-invert md:px-0">
-            {children}
-            <hr className="mb-6 mt-6 w-full rounded-full border-gray-6 md:mt-12" />
-            <DesignPageNav pageSlug={selected} />
-          </article>
-        </MDXProvider>
-      </ContainerLayout>
-    </BaseLayout>
+    <>
+      <NextSeo
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          title: `5/9 Design | ${name}`,
+        }}
+      />
+
+      <BaseLayout name={name} pageSlug="/design">
+        {/* Note: `pb-6` overrides `pb-4` on small devices. `<DesignNavBar />`
+            has a `mb-6` when displayed on small screens, so the ``margin''
+            above/below the article content is symmetrical. We do this instead
+            of `py-6` to correctly position `<DesignNavBar />`, as well as the
+            article content on small screens. This positioning issue is not
+            present on larger screens so we have a breakpoint to reset it. For
+            similar reasons, the `x` padding is set to 0 on small devices is set
+            to 0. */}
+        <ContainerLayout className="relative flex max-w-[90rem] flex-col space-x-0 px-0 pb-6 pt-0 md:flex-row md:space-x-16">
+          <DesignNavBar selected={selected} />
+          <MDXProvider components={components}>
+            <article className="prose prose-gray max-w-none grow px-4 dark:prose-invert md:px-0">
+              {children}
+              <hr className="mb-6 mt-6 w-full rounded-full border-gray-6 md:mt-12" />
+              <DesignPageNav pageSlug={selected} />
+            </article>
+          </MDXProvider>
+        </ContainerLayout>
+      </BaseLayout>
+    </>
   );
 };
 
