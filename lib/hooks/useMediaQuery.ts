54fea45eb128bda4ec86533affa1f20d4b818e1d
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-06-17 01:03:16 +0900

    fix: allow pointer events inside navbar + refactor

---
+++ lib/hooks/useMediaQuery.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+// Use this to query media elements (e.g. screen size)
+export const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+
+    const listener = () => {
+      setMatches(media.matches);
+    };
+
+    media.addEventListener('change', listener);
+    return () => media.removeEventListener('change', listener);
+  }, [matches, query]);
+
+  return matches;
+};
