951d69ab8b63d32dfb021db89790f6a705cda50a
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-06-20 08:53:30 -0400

    chore: remove inter medium

--- pages/api/og/design.tsx
+++ pages/api/og/design.tsx
@@ -215,12 +215,12 @@ export default async function handler(req: NextRequest) {
           style: 'normal',
           weight: 400,
         },
-        {
+        /* {
           name: 'Inter',
           data: interMediumFont,
           style: 'normal',
           weight: 500,
-        },
+        }, */
         {
           name: 'Inter',
           data: interSemiBoldFont,
