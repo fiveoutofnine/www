261d2797a11b73e1387778acf48fa77e8bb285c0
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-06-28 00:11:01 -0400

    docs: explain what the obfuscated script prefix does

--- pages/api/chess/asset/[id].ts
+++ pages/api/chess/asset/[id].ts
@@ -22,7 +22,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   res.setHeader('Content-Type', 'text/html');
   // Cache response for 1 week, revalidate after 1 day.
   res.setHeader('cache-control', 'public, s-maxage=604800, stale-while-revalidate=86400');
-  res.status(200)
-    .send(`<script type="text/javascript">w=window;w.addEventListener('DOMContentLoaded',()=>{n=document.querySelector('section').style;n.transformOrigin='top left';a=()=>n.transform='scale('+w.innerWidth/1000+')';a();w.onresize=a});
-  </script>${Buffer.from(data.animation_url.substring(22), 'base64').toString()}`);
+  // The prepended script tag is to scale the animation to the window size. It
+  // is equivalent to the following:
+  // ```js
+  // window.addEventListener('DOMContentLoaded', () => {
+  //   const section = document.querySelector('section');
+  //   section.style.transformOrigin = 'top left';
+  //   const scale = () => section.style.transform = 'scale(' + window.innerWidth / 1000 + ')';
+  //   scale();
+  //   window.onresize = scale;
+  // });
+  // ```
+  res
+    .status(200)
+    .send(
+      `<script type="text/javascript">w=window;w.addEventListener('DOMContentLoaded',()=>{n=document.querySelector('section').style;n.transformOrigin='top left';a=()=>n.transform='scale('+w.innerWidth/1000+')';a();w.onresize=a});</script>${Buffer.from(
+        data.animation_url.substring(22),
+        'base64',
+      ).toString()}`,
+    );
 }
