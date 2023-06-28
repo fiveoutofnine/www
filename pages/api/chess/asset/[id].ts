22e8accac7610f1d52dd5d157ce17f2c8c8a1505
5/9 <66016924+fiveoutofnine@users.noreply.github.com>
2023-06-28 00:59:36 -0400

    Implement fiveoutofnine chess NFT `tokenURI` proxy endpoint (#17)
    * feat: implement 5/9 tokenURI proxy endpoint
    
    * fix: move supabase admin client to server
    
    * docs: explain what the obfuscated script prefix does
    
    * chore: update base url
    
    * chore: update base url
    
    ---------
    
    Co-authored-by: e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>

---
+++ pages/api/chess/asset/[id].ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { idSchema } from '@/lib/schemas';
+import supabase from '@/lib/services/supabase';
+import { validateQuery } from '@/lib/utils';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<string>) {
+  const { id } = validateQuery(idSchema, req.query);
+
+  // Fetch image.
+  const { data, status, error } = await supabase
+    .from('chess_nft_metadata')
+    .select('animation_url')
+    .eq('id', id)
+    .single();
+
+  if ((error && status !== 406) || !data) {
+    res.status(404).send('Not found.');
+    return;
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  // Cache response for 1 week, revalidate after 1 day.
+  res.setHeader('cache-control', 'public, s-maxage=604800, stale-while-revalidate=86400');
+  // The prepended script tag is to scale the animation to the window size. It
+  // is equivalent to the following:
+  // ```js
+  // window.addEventListener('DOMContentLoaded', () => {
+  //   const section = document.querySelector('section');
+  //   section.style.transformOrigin = 'top left';
+  //   const scale = () => section.style.transform = 'scale(' + window.innerWidth / 1000 + ')';
+  //   scale();
+  //   window.onresize = scale;
+  // });
+  // ```
+  res
+    .status(200)
+    .send(
+      `<script type="text/javascript">w=window;w.addEventListener('DOMContentLoaded',()=>{n=document.querySelector('section').style;n.transformOrigin='top left';a=()=>n.transform='scale('+w.innerWidth/1000+')';a();w.onresize=a});</script>${Buffer.from(
+        data.animation_url.substring(22),
+        'base64',
+      ).toString()}`,
+    );
+}
---
+++ pages/api/chess/token/[id].ts
@@ -0,0 +1,90 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { createClient } from '@supabase/supabase-js';
+import { BigNumber } from 'ethers';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
+
+import { idSchema } from '@/lib/schemas';
+import type { ChessNFTMetadata } from '@/lib/types/chess';
+import { validateQuery } from '@/lib/utils';
+
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY,
+);
+
+const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ChessNFTMetadata | Error>,
+) {
+  const { id } = validateQuery(idSchema, req.query);
+
+  // First, attempt to fetch the metadata from Supabase.
+  const { data, status, error } = await supabaseAdmin
+    .from('chess_nft_metadata')
+    .select('*')
+    .eq('id', id)
+    .returns<ChessNFTMetadata[]>();
+
+  let metadata: ChessNFTMetadata;
+  // If there is an error, or if the data is empty, or if there is no data,
+  // then attempt to fetch the metadata from Ethereum via the `_tokenURI`
+  // function.
+  if ((error && status !== 406) || (data && data.length === 0) || !data) {
+    try {
+      // Fetch metadata.
+      const tokenURI = await publicClient.readContract({
+        // fiveoutofnine Chess NFT contract
+        address: '0xB543F9043b387cE5B3d1F0d916E42D8eA2eBA2E0',
+        abi: [
+          {
+            inputs: [{ internalType: 'uint256', name: '_tokenId', type: 'uint256' }],
+            name: '_tokenURI',
+            outputs: [{ internalType: 'string', name: '', type: 'string' }],
+            stateMutability: 'view',
+            type: 'function',
+          },
+        ],
+        functionName: '_tokenURI',
+        args: [BigNumber.from(id)],
+      });
+      const tokenURIParsed = JSON.parse(
+        Buffer.from(tokenURI.substring(29), 'base64').toString(),
+      ) as ChessNFTMetadata;
+
+      metadata = {
+        id,
+        name: tokenURIParsed.name,
+        description: tokenURIParsed.description,
+        animation_url: tokenURIParsed.animation_url,
+        attributes: tokenURIParsed.attributes,
+      };
+    } catch (err) {
+      res.status(404).json({ name: 'Not found', message: 'Token is nonexistent.' });
+      return;
+    }
+
+    // Insert into Supabase.
+    const { error: upsertError } = await supabaseAdmin.from('chess_nft_metadata').upsert(metadata);
+    if (upsertError) {
+      res.status(500).json({ name: 'Internal server error', message: 'Something went wrong.' });
+      return;
+    }
+  } else {
+    metadata = data[0];
+  }
+
+  // Overwrite `animation_url` to proxied version on our server to accomodate
+  // marketplaces' CSPs.
+  metadata.animation_url = `https://fiveoutofnine.com/api/chess/asset/${id}`;
+
+  // Cache response for 30 days.
+  res.setHeader('cache-control', 'public, s-maxage=2592000');
+  res.status(200).json(metadata);
+}
