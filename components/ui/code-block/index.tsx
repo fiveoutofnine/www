6956a26ce609a26805af4fa81bdc6b103c107e8d
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-06-30 11:42:15 -0400

    feat: copy to clipboard fn

--- components/ui/code-block/index.tsx
+++ components/ui/code-block/index.tsx
@@ -1,8 +1,8 @@
-import type { FC } from 'react';
+import { type FC, useState } from 'react';
 
 import type { CodeBlockProps } from './types';
 import clsx from 'clsx';
-import { Copy } from 'lucide-react';
+import { Check, Copy } from 'lucide-react';
 import { Highlight, themes } from 'prism-react-renderer';
 
 import { IconButton } from '@/components/ui';
@@ -15,14 +15,30 @@ const CodeBlock: FC<CodeBlockProps> = ({
   children,
   ...rest
 }) => {
-  // TODO: show clipboard button (+ implement when there's no filename)
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const copyToClipboard = () => {
+    if (!copied) {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    }
+    navigator.clipboard.writeText(children);
+  };
+
   return (
     <div className="flex flex-col overflow-hidden rounded-xl border border-gray-6">
       {fileName !== undefined ? (
         <div className="flex h-10 grow items-center justify-between border-b border-gray-6 bg-gray-2 pl-4 pr-2">
           <div className="text-sm text-gray-11">{fileName}</div>
-          <IconButton size="sm" variant="outline" type="button">
-            <Copy />
+          <IconButton
+            size="sm"
+            variant="outline"
+            title="Copy to clipboard"
+            onClick={copyToClipboard}
+            type="button"
+            aria-label="Copy to clipboard"
+          >
+            {copied ? <Check /> : <Copy />}
           </IconButton>
         </div>
       ) : null}
