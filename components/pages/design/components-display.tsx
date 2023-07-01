4fb8890345095c6cd2d5ff9c4d3121322efbd6ec
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-07-01 19:14:59 -0400

    fix: use prettier+babel to format code

--- components/pages/design/components-display.tsx
+++ components/pages/design/components-display.tsx
@@ -1,6 +1,8 @@
 import { Children, type FC, isValidElement, type ReactNode, useCallback, useMemo } from 'react';
 
 import clsx from 'clsx';
+import prettier from 'prettier';
+import babel from 'prettier/parser-babel';
 import { twMerge } from 'tailwind-merge';
 
 import { CodeBlock } from '@/components/ui';
@@ -10,48 +12,41 @@ const DesignComponentsDisplay: FC<JSX.IntrinsicElements['div']> = ({
   children,
   ...rest
 }) => {
-  const getJsxString = useCallback(
-    ({ node, depth = 0 }: { node: ReactNode; depth: number }): string => {
-      const indents = '  '.repeat(depth + 1);
-      if (!isValidElement(node)) return `${indents}${node}`.trimEnd();
+  const getJsxString = useCallback((node: ReactNode): string => {
+    if (!isValidElement(node)) return `${node}`.trimEnd();
 
-      let children: ReactNode | undefined | null;
-      const componentName = typeof node.type !== 'string' ? node.type.name : 'Unknown';
-      const propArray = Object.entries(node.props).map((prop) => {
+    let children: ReactNode | undefined | null;
+    const componentName = typeof node.type !== 'string' ? node.type.name : 'Unknown';
+    const propString = Object.entries(node.props)
+      .map((prop) => {
         if (prop[0] === 'children') {
           children = prop[1] as ReactNode;
           return;
         }
         if (typeof prop[1] === 'string') return `${prop[0]}="${prop[1]}"`;
-        else if (isValidElement(prop[1]))
-          return `${prop[0]}={${getJsxString({ node: prop[1], depth: depth + 1 }).trim()}}`;
+        else if (isValidElement(prop[1])) return `${prop[0]}={${getJsxString(prop[1])}}`;
         return `${prop[0]}={${prop[1]}}`;
-      });
-      const propString =
-        propArray.length < 5
-          ? propArray.join(' ').trim()
-          : `\n${indents}  ${propArray.join(`\n${indents}  `)}`;
+      })
+      .join(' ')
+      .trim();
 
-      const componentChildren = Children.toArray(children);
-      const childrenJsxString = componentChildren
-        .map((child) => getJsxString({ node: child, depth: depth + 1 }))
-        .join('\n');
+    const componentChildren = Children.toArray(children);
+    const childrenJsxString = componentChildren.map((child) => getJsxString(child)).join();
 
-      return componentChildren.length > 0
-        ? `${indents}<${componentName} ${propString}>\n${childrenJsxString}\n${indents}</${componentName}>`.trimEnd()
-        : `${indents}<${componentName} ${propString}/>\n`.trimEnd();
-    },
-    [],
-  );
+    return componentChildren.length > 0
+      ? `<${componentName} ${propString}>${childrenJsxString}</${componentName}>`
+      : `<${componentName} ${propString}/>`;
+  }, []);
 
   const code = useMemo(() => {
     const componentChildren = Children.toArray(children).filter((child) => isValidElement(child));
 
-    return `<DesignComponentsDisplay${
-      className ? ` className="${className}"` : ''
-    }>\n${componentChildren
-      .map((child) => getJsxString({ node: child, depth: 0 }))
-      .join('\n')}\n</DesignComponentsDisplay>`;
+    return prettier.format(
+      `<DesignComponentsDisplay${className ? ` className="${className}"` : ''}>\n${componentChildren
+        .map((child) => getJsxString(child))
+        .join('\n')}</DesignComponentsDisplay>`,
+      { parser: 'babel', plugins: [babel] },
+    );
   }, [children, className, getJsxString]);
 
   return (
