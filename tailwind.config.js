ef723543bcfada2c8a10c7af37a30ba1eb34193c
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-07-02 08:40:00 -0400

    fix: add import

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  /* darkMode: 'class', */
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
