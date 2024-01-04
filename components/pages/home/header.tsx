9a9c26c672752d8fe1f445357a562c5565055e43
Kent Miguel <kentmiguel10@gmail.com>
2024-01-04 13:47:16 -0500

    Added God Rays styling

--- components/pages/home/header.tsx
+++ components/pages/home/header.tsx
@@ -1,24 +1,28 @@
+import { usePathname } from 'next/navigation';
 import type { FC } from 'react';
 
-import { Github, Twitter } from 'lucide-react';
+import { Download, Github, Linkedin, Twitter } from 'lucide-react';
 
-import KentMiguelAvatar from '@/components/common/fiveoutofnine-avatar';
+import KentMiguelAvatar from '@/components/common/kentmiguel-avatar';
 import LinkPreview from '@/components/templates/link-preview';
 import { Button } from '@/components/ui';
 
 const KentMiguelHeader: FC = () => {
+  const currentPath = usePathname();
   return (
-    <div className="flex flex-col items-center rounded-xl border border-gray-6 bg-gray-2 p-3 md:flex-row md:justify-between md:rounded-2xl md:p-6">
-      <div className="flex w-full items-center">
-        {/* Avatar (desktop) */}
-        <KentMiguelAvatar className="mr-4 hidden md:block" size={56} />
-        {/* Avatar (mobile) */}
-        <KentMiguelAvatar className="mr-2 md:hidden" size={40} />
-        <div>
-          <div className="text-base font-semibold md:text-2xl">Kent Miguel</div>
-          <span className="mt-0.5 text-sm text-gray-11 md:mt-1 md:text-base">
-            Working on{' '}
-            <LinkPreview
+    <div className="relative flex flex-col items-center rounded-xl border border-gray-6 bg-gray-2 p-3 md:flex-row md:justify-between md:rounded-2xl md:p-6">
+      <div className="jumbo absolute inset-0 z-0 opacity-50"></div>
+      <div className="relative z-10 flex h-full w-full items-center">
+        <div className="flex w-full items-center">
+          {/* Avatar (desktop) */}
+          <KentMiguelAvatar className="mr-4 hidden md:block" size={56} />
+          {/* Avatar (mobile) */}
+          <KentMiguelAvatar className="mr-2 md:hidden" size={40} />
+          <div>
+            <div className="text-base font-semibold md:text-2xl">Kent Miguel</div>
+            <span className="mt-0.5 text-sm text-gray-11 md:mt-1 md:text-base">
+              Working on{' '}
+              {/* <LinkPreview
               className="font-medium text-gray-12 hover:underline"
               href="https://waterfall.market"
               src="/static/waterfall-market-preview.webp"
@@ -37,44 +41,83 @@ const KentMiguelHeader: FC = () => {
             >
               Curta
             </LinkPreview>
-            .
-          </span>
+            . */}
+            </span>
+          </div>
         </div>
       </div>
 
-      {/* Links (desktop) */}
+      {/* Links (Desktop) */}
       <div className="hidden space-x-2 md:flex">
         <Button
           intent="primary"
-          href="https://twitter.com/fiveoutofnine"
-          leftIcon={<Twitter />}
+          href="https://www.linkedin.com/in/kent-miguel/"
+          leftIcon={<Linkedin />}
           newTab
         >
-          Twitter
+          LinkedIn
         </Button>
-        <Button href="https://github.com/fiveoutofnine" leftIcon={<Github />} newTab>
+        <Button
+          href="https://github.com/kmiguel10"
+          leftIcon={<Github />}
+          intent="success"
+          variant="primary"
+          newTab
+        >
           GitHub
         </Button>
+        {currentPath === '/resume' && (
+          <Button
+            size="md"
+            className="w-full"
+            leftIcon={<Download />}
+            intent="success"
+            variant="outline"
+            newTab
+          >
+            <a href="/kentmiguel.pdf" download>
+              Resume
+            </a>
+          </Button>
+        )}
       </div>
-      {/* Links (mobile) */}
+      {/* Links (Mobile) */}
       <div className="mt-4 flex w-full space-x-2 md:hidden">
         <Button
           size="md"
           intent="primary"
           className="w-full"
-          href="https://twitter.com/fiveoutofnine"
-          leftIcon={<Twitter />}
+          href="https://www.linkedin.com/in/kent-miguel/"
+          leftIcon={<Linkedin />}
+          newTab
         >
-          Twitter
+          LinkedIn
         </Button>
         <Button
           size="md"
           className="w-full"
-          href="https://github.com/fiveoutofnine"
+          intent="success"
+          variant="primary"
+          href="https://github.com/kmiguel10"
           leftIcon={<Github />}
+          newTab
         >
           GitHub
         </Button>
+        {currentPath === '/resume' && (
+          <Button
+            size="md"
+            className="w-full"
+            leftIcon={<Download />}
+            intent="success"
+            variant="outline"
+            newTab
+          >
+            <a href="/kentmiguel.pdf" download>
+              Resume
+            </a>
+          </Button>
+        )}
       </div>
     </div>
   );
