76cbbe762b75ac8d6a40f5b827ec30c6f1b4c6a1
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-04-06 11:19:05 -0400

    feat: add link features

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import ChessFeature from './(components)/works/chess';
 
 import ContainerLayout from '@/components/layouts/container';
+import BitTwiddlingFeature from '@/components/pages/home/featured-works/works/bit-twiddling';
+import CoolContractsFeature from '@/components/pages/home/featured-works/works/cool-contracts';
 import FiveoutofnineHeader from '@/components/pages/home/header';
 
 export default function Page() {
@@ -10,6 +12,8 @@ export default function Page() {
       {/* Featured works. */}
       <div className="grid grid-cols-2 gap-4 min-[560px]:grid-cols-4 min-[960px]:grid-cols-6">
         <ChessFeature />
+        <BitTwiddlingFeature />
+        <CoolContractsFeature />
       </div>
     </ContainerLayout>
   );
