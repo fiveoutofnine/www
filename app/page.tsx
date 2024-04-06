27e525d317755f4f611b6ec77fd1440e0191d05d
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-04-06 11:01:44 -0400

    feat: `<ChessFeature />`

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import ChessFeature from './(components)/works/chess';
+
 import ContainerLayout from '@/components/layouts/container';
 import FiveoutofnineHeader from '@/components/pages/home/header';
 
 export default function Page() {
   return (
-    <ContainerLayout>
+    <ContainerLayout className="flex flex-col space-y-4">
       <FiveoutofnineHeader />
+      {/* Featured works. */}
+      <div className="grid grid-cols-2 gap-4 min-[560px]:grid-cols-4 min-[960px]:grid-cols-6">
+        <ChessFeature />
+      </div>
     </ContainerLayout>
   );
 }
