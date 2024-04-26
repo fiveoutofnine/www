c04625c2dec9a2298fc9d4cfe966fbc5b8ac2a50
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-04-26 09:28:45 -0400

    feat: add `<TxDotCoolFeature />` w/o functionality

--- app/(components)/works/tx-dot-cool/detail.tsx
+++ app/(components)/works/tx-dot-cool/detail.tsx
@@ -1,49 +1,14 @@
-import { type FC, useEffect, useRef, useState } from 'react';
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
 
 import { ArrowUp, ExternalLink, MessageCircle } from 'lucide-react';
 
 import { FIVEOUTOFNINE_MESSAGES } from '@/lib/constants/on-chain-messages';
 
-import CategoryTag from '@/components/templates/category-tag';
-import FeatureDisplay from '@/components/templates/feature-display';
-import { Button, IconButton } from '@/components/ui';
+import { IconButton } from '@/components/ui';
 
-const TxDotCoolFeature: FC = () => {
-  return (
-    <FeatureDisplay
-      className="col-span-2 w-full min-[960px]:w-64"
-      name="tx.cool"
-      description="UI/UX explorations"
-      symbol={
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-        >
-          <title>tx.cool</title>
-          <desc>tx.cool&apos;s logo</desc>
-          <path
-            d="M9.625 7.455v3.267H1.359V7.455h8.266ZM2.96 4.449h4.51v11.516c0 .174.03.322.09.441a.59.59 0 0 0 .277.262c.126.054.286.081.482.081.136 0 .294-.016.474-.049a6.56 6.56 0 0 0 .408-.081l.654 3.169c-.202.06-.49.133-.866.22-.37.087-.811.145-1.323.172-1.035.054-1.903-.052-2.606-.319-.702-.272-1.23-.7-1.584-1.282-.354-.583-.526-1.312-.515-2.19V4.45Zm11.173 3.006 1.83 3.887 1.928-3.887h4.476L19 13.727 22.53 20h-4.443l-2.124-3.986L13.905 20H9.396l3.561-6.273-3.332-6.272h4.508Z"
-            fill="currentColor"
-          />
-        </svg>
-      }
-      button={
-        <Button size="sm" href="https://tx.cool" rightIcon={<ExternalLink />} newTab>
-          Visit
-        </Button>
-      }
-      tags={[<CategoryTag key={0} category="Web" />]}
-    >
-      <TxDotCoolFeatureDetail />
-    </FeatureDisplay>
-  );
-};
-
-const TxDotCoolFeatureDetail: FC = () => {
+const TxDotCoolFeatureDetail: React.FC = () => {
   const [userMessage, setUserInput] = useState<string>('');
 
   const messagesEndRef = useRef<HTMLDivElement>(null);
@@ -139,7 +104,7 @@ const TxDotCoolFeatureDetail: FC = () => {
         {/* Header */}
         <div className="sticky top-0 flex h-8 items-center justify-between border-b border-gray-6 bg-gray-3 px-1.5">
           <div className="flex items-center space-x-1">
-            <MessageCircle className="h-4 w-4 text-gray-11" />
+            <MessageCircle className="size-4 text-gray-11" />
             <div className="font-medium text-gray-12">fiveoutofnine.eth</div>
           </div>
           <IconButton
@@ -155,7 +120,7 @@ const TxDotCoolFeatureDetail: FC = () => {
           {FIVEOUTOFNINE_MESSAGES.map((message) => (
             <a
               key={message.txHash}
-              className="flex min-h-[1.5rem] w-fit items-center rounded-xl bg-gray-7 px-2 py-1 transition-colors hover:bg-gray-8"
+              className="flex min-h-[1.5rem] w-fit items-center rounded-xl bg-gray-7 px-2 py-1 text-gray-12 transition-colors hover:bg-gray-8"
               href={`https://etherscan.io/tx/${message.txHash}`}
               target="_blank"
               rel="noopener noreferrer"
@@ -204,4 +169,4 @@ const TxDotCoolFeatureDetail: FC = () => {
   );
 };
 
-export default TxDotCoolFeature;
+export default TxDotCoolFeatureDetail;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import FiveoutofnineHeader from './(components)/header';
 import BitTwiddlingFeature from './(components)/works/bit-twiddling';
 import ChessFeature from './(components)/works/chess';
 import CoolContractsFeature from './(components)/works/cool-contracts';
+import TxDotCoolFeature from './(components)/works/tx-dot-cool';
 
 import ContainerLayout from '@/components/layouts/container';
 
@@ -12,6 +13,7 @@ export default function Page() {
       {/* Featured works. */}
       <div className="grid grid-cols-2 gap-4 min-[560px]:grid-cols-4 min-[960px]:grid-cols-6">
         <ChessFeature />
+        <TxDotCoolFeature />
         <BitTwiddlingFeature />
         <CoolContractsFeature />
       </div>
