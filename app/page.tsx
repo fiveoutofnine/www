f536a48dc7ac7d15e691570ce5228342ded4b7d6
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-07 13:14:18 -0400

    chore: ini `<RunningFeature />`

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import BitTwiddlingFeature from './(components)/works/bit-twiddling';
 import ChessFeature from './(components)/works/chess';
 import ColormapRegistryFeature from './(components)/works/colormap-registry';
 import CoolContractsFeature from './(components)/works/cool-contracts';
+import RunningFeature from './(components)/works/running';
 import TxDotCoolFeature from './(components)/works/tx-dot-cool';
 import TypingFeature from './(components)/works/typing';
 
@@ -12,11 +13,11 @@ export default function Page() {
   return (
     <ContainerLayout className="flex flex-col space-y-4">
       <FiveoutofnineHeader />
-      {/* Featured works. */}
       <div className="grid grid-cols-2 gap-4 min-[560px]:grid-cols-4 min-[960px]:grid-cols-6">
         <ChessFeature />
         <TxDotCoolFeature />
         <TypingFeature />
+        <RunningFeature />
         <ColormapRegistryFeature />
         <BitTwiddlingFeature />
         <CoolContractsFeature />
