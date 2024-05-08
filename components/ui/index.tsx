bed2d74e78701c75b96f92e5490d5a489f3df14a
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-08 12:36:43 -0400

    feat: add `<Input />`

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -6,6 +6,7 @@ export { default as Checkbox } from './checkbox';
 export { default as CodeBlock } from './code-block';
 export { default as HoverCard } from './hover-card';
 export { default as IconButton } from './icon-button';
+export { default as Input } from './input';
 export { default as Radio } from './radio';
 export { default as Select } from './select';
 export { default as Switch } from './switch';
---
+++ components/ui/input/types.tsx
@@ -0,0 +1,37 @@
+import { inputVariants } from './styles';
+import type { VariantProps } from 'class-variance-authority';
+
+// -----------------------------------------------------------------------------
+// Variant props
+// -----------------------------------------------------------------------------
+
+type InputVariantProps = VariantProps<typeof inputVariants>;
+
+// -----------------------------------------------------------------------------
+// Component props
+// -----------------------------------------------------------------------------
+
+export type InputProps = Omit<JSX.IntrinsicElements['input'], 'size'> &
+  InputVariantProps &
+  (
+    | {
+        prefix?: undefined;
+        leftIcon?: React.ReactNode;
+      }
+    | {
+        prefix?: React.ReactNode;
+        leftIcon?: undefined;
+      }
+  ) &
+  (
+    | {
+        suffix?: undefined;
+        rightIcon?: React.ReactNode;
+      }
+    | {
+        suffix?: React.ReactNode;
+        rightIcon?: undefined;
+      }
+  ) & {
+    containerized?: boolean;
+  };
