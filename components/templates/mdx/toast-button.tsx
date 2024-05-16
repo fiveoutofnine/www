dee79c0ac9dd3b950bddb3aef846ef3b1e3f558f
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-17 07:44:53 +0900

    feat: use existing `ToastProps`

--- components/templates/mdx/toast-button.tsx
+++ components/templates/mdx/toast-button.tsx
@@ -1,37 +1,39 @@
 'use client';
 
 import { Button, toast } from '@/components/ui';
+import type { ToastProps } from '@/components/ui/toast/types';
 
 // -----------------------------------------------------------------------------
 // Props
 // -----------------------------------------------------------------------------
 
-type ToastButtonProps = {
-  title?: string;
-  description: string;
-  intent?: 'none' | 'info' | 'success' | 'fail' | 'warning' | 'orange';
-  action?: React.ReactElement;
+type ToastButtonProps = ToastProps & {
+  children?: React.ReactNode;
 };
 
 // -----------------------------------------------------------------------------
 // Component
 // -----------------------------------------------------------------------------
 
-const ToastButton: React.FC<ToastButtonProps> = ({
-  title = 'Title',
-  description = 'Swipe right to remove.',
-  intent,
-  action,
-  ...rest
-}) => {
+const ToastButton: React.FC<ToastButtonProps> = ({ children, intent, ...toastPropsRest }) => {
+  const toastProps: ToastProps = {
+    intent: intent || 'info',
+    title: toastPropsRest.title || 'Title',
+    description: toastPropsRest.description || 'Some short description.',
+    hasCloseButton: toastPropsRest.hasCloseButton || true,
+    ...toastPropsRest,
+  };
+
   return (
-    <Button
-      intent={intent}
-      onClick={() => toast({ title, description, intent, action })}
-      {...rest}
-    />
+    <Button intent={intent} onClick={() => toast({ ...toastProps })}>
+      {children}
+    </Button>
   );
 };
+
+// -----------------------------------------------------------------------------
+// Export
+// -----------------------------------------------------------------------------
 
 ToastButton.displayName = 'ToastButton';
 
