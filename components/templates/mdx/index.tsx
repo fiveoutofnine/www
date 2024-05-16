81fc52041a220b49167e203e25f2cd4651bcb768
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-16 15:11:39 +0900

    feat: add design component display accordion

--- components/templates/mdx/index.tsx
+++ components/templates/mdx/index.tsx
@@ -5,3 +5,4 @@ export { default as H1 } from './h1';
 export { default as H2 } from './h2';
 export { default as H3 } from './h3';
 export { default as P } from './p';
+export { default as ToastButton } from './toast-button';
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,9 @@
 import { isValidElement } from 'react';
 
+import DesignComponentsDisplay from './app/design/(components)/components-display';
 import type { MDXComponents } from 'mdx/types';
 
-import { A, Code, H1, H2, H3, P } from '@/components/templates/mdx';
+import { A, Code, H1, H2, H3, P, ToastButton } from '@/components/templates/mdx';
 import { CodeBlock } from '@/components/ui';
 import type { CodeBlockProps } from '@/components/ui/code-block/types';
 
@@ -29,6 +30,8 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         </CodeBlock>
       );
     },
+    DesignComponentsDisplay,
+    ToastButton,
     ...components,
   };
 }
