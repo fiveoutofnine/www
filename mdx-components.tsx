fc4024e62fd655f731ef5a08f930ad26854ac184
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-16 13:13:19 +0900

    feat: add `<pre>`

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,10 @@
+import { isValidElement } from 'react';
+
 import type { MDXComponents } from 'mdx/types';
 
 import { H1, H2, H3 } from '@/components/templates/mdx';
+import { CodeBlock } from '@/components/ui';
+import type { CodeBlockProps } from '@/components/ui/code-block/types';
 
 // This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
@@ -8,6 +12,20 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h1: (props: JSX.IntrinsicElements['h1']) => <H1 {...props} />,
     h2: (props: JSX.IntrinsicElements['h2']) => <H2 {...props} />,
     h3: (props: JSX.IntrinsicElements['h3']) => <H3 {...props} />,
+    pre: ({
+      children,
+      ...rest
+    }: JSX.IntrinsicElements['pre'] & Omit<CodeBlockProps, 'children'>) => {
+      const childrenProps = isValidElement(children) ? children.props : undefined;
+      const language = childrenProps?.className ? childrenProps.className.substring(9) : undefined;
+      const code = typeof childrenProps?.children === 'string' ? childrenProps.children.trim() : '';
+
+      return (
+        <CodeBlock language={language} {...rest}>
+          {code}
+        </CodeBlock>
+      );
+    },
     ...components,
   };
 }
