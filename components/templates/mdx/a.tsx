a05abbd46a9f61d1043c6b9200384a069eff45e7
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-17 16:59:43 +0900

    fix: `code` styles

--- components/templates/mdx/a.tsx
+++ components/templates/mdx/a.tsx
@@ -4,7 +4,7 @@ const A: React.FC<JSX.IntrinsicElements['a']> = ({ href, children, ...rest }) =>
   if (href && href.startsWith('/')) {
     return (
       <Link
-        className="mdx--link group font-medium text-blue-9 no-underline hover:underline"
+        className="mdx--link group font-medium text-blue-9 no-underline hover:underline has-[code]:focus-visible:ring-0"
         href={href}
       >
         {children}
@@ -14,7 +14,7 @@ const A: React.FC<JSX.IntrinsicElements['a']> = ({ href, children, ...rest }) =>
 
   return (
     <a
-      className="mdx--link group w-fit font-medium text-blue-9 no-underline hover:underline"
+      className="mdx--link group w-fit font-medium text-blue-9 no-underline hover:underline has-[code]:focus-visible:ring-0"
       href={href}
       target="_blank"
       rel="noopener noreferrer"
--- components/templates/mdx/code.tsx
+++ components/templates/mdx/code.tsx
@@ -1,7 +1,7 @@
 const Code: React.FC<JSX.IntrinsicElements['code']> = (props) => {
   return (
     <code
-      className="rounded border border-gray-6 bg-gray-3 px-1 py-0.5 text-sm font-normal text-gray-12 before:content-none after:content-none group-[.mdx--link]:text-blue-9"
+      className="rounded-md border border-gray-6 bg-gray-3 px-1 text-[0.875em] font-normal leading-[1.15] text-gray-12 before:content-none after:content-none group-focus-visible:ring-2 group-focus-visible:ring-blue-9 group-[.mdx--link]:text-blue-9"
       {...props}
     />
   );
