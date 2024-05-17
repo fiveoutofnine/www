ced74b61e0e16c7e1addab620ceadcc4c50e4c7b
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-18 08:04:36 +0900

    docs: edit `/design/badge` content

--- components/templates/mdx/p.tsx
+++ components/templates/mdx/p.tsx
@@ -1,7 +1,7 @@
 const P: React.FC<JSX.IntrinsicElements['p']> = (props) => {
   return (
     <p
-      className="font-light not-italic text-gray-11 before:content-none after:content-none group-[.mdx--callout]:my-0 group-[.mdx--callout]:text-blue-12"
+      className="font-light not-italic text-gray-11 before:content-none after:content-none group-[.mdx--callout]:my-0 group-[.mdx--callout]:text-inherit"
       {...props}
     />
   );
