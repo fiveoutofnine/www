7538a2871f06b576862add5e7ea3055a70a9909d
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-30 11:30:35 +0900

    feat: register `<Radio />` on `/design` page

---
+++ app/design/(components)/radio-feature.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Radio } from '@/components/ui';
+
+const DesignRadioFeature: React.FC = () => {
+  return (
+    <Radio.Group>
+      <div className="flex items-center gap-1.5">
+        <Radio.Item id="radio_feature_0" value="0" />
+        <label htmlFor="radio_feature_0" className="text-sm font-medium text-gray-12">
+          Radio
+        </label>
+      </div>
+    </Radio.Group>
+  );
+};
+
+export default DesignRadioFeature;
