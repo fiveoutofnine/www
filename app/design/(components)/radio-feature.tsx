61dd13d8bd4492d7132473fc7294421c31aa6879
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-05-30 11:57:36 +0900

    chore: fix ordering and add labels

--- app/design/(components)/radio-feature.tsx
+++ app/design/(components)/radio-feature.tsx
@@ -6,8 +6,12 @@ const DesignRadioFeature: React.FC = () => {
   return (
     <Radio.Group>
       <div className="flex items-center gap-1.5">
-        <Radio.Item id="radio_feature_0" value="0" />
-        <label htmlFor="radio_feature_0" className="text-sm font-medium text-gray-12">
+        <Radio.Item id="radio_feature_0" value="0" aria-labelledby="radio_feature_0_label" />
+        <label
+          id="radio_feature_0_label"
+          htmlFor="radio_feature_0"
+          className="text-sm font-medium text-gray-12"
+        >
           Radio
         </label>
       </div>
