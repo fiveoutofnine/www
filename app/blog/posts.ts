3daee27f523b494b59cfd16295efa255149fd041
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-06-03 22:44:32 +0900

    chore: update posts

--- app/blog/posts.ts
+++ app/blog/posts.ts
@@ -1,17 +1,8 @@
 export const POSTS: { slug: string; title: string; date: Date }[] = [
+  // 2022
   {
-    slug: '555-reasons-sudolabel-types-slow',
-    title: '555 reasons sudolabel types slow',
-    date: new Date('12/12/2024'),
-  },
-  {
-    slug: '55-reasons-sudolabel-runs-slow',
-    title: '55 reasons sudolabel runs slow',
-    date: new Date('11/12/2024'),
-  },
-  {
-    slug: '5-reasons-sudolabel-runs-a-half-marathon-slowly',
-    title: '5 reasons sudolabel runs a half marathon slowly',
-    date: new Date('12/12/2022'),
+    slug: 'basic-bit-manipulation',
+    title: 'Basic Bit Manipulation',
+    date: new Date('2022-04-06T20:43:33.436Z'),
   },
 ];
