5cc7b51a65643b9e508222302cf7da52e0fcebe5
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-06-05 10:21:38 +0900

    feat: ini blog og image

--- lib/utils/getBlogPageMetadata.ts
+++ lib/utils/getBlogPageMetadata.ts
@@ -9,8 +9,15 @@ const getBlogPageMetadata = ({
   slug: string;
   description?: string;
 }): Metadata => {
-  // const images = [];
-  const title = POSTS.find((post) => post.slug === slug)?.title || '5/9 Blog';
+  const title = POSTS.find((post) => post.slug === slug)?.title || '5/9 Blog Post';
+  const images = [
+    {
+      url: `https://fiveoutofnine.com/api/og/design?title=${title}&description=${description}`,
+      alt: `Open Graph image for ${title}`,
+      width: 1200,
+      height: 630,
+    },
+  ];
 
   return {
     title: {
@@ -22,7 +29,7 @@ const getBlogPageMetadata = ({
     openGraph: {
       title,
       description,
-      // images,
+      images,
       url: `https://fiveoutofnine.com/blog/${slug}`,
       siteName: 'fiveoutofnine',
       locale: 'en_US',
@@ -31,7 +38,7 @@ const getBlogPageMetadata = ({
     twitter: {
       title,
       description,
-      // images,
+      images,
       card: 'summary_large_image',
       creator: '@fiveoutofnine',
       creatorId: '1269561030272643076',
