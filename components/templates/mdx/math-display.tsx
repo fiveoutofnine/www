ea064920518ced6a8793acec49884913d76298db
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-06-05 09:54:11 +0900

    fix: make `MathDisplay` and tables fullbleed on `md`

--- components/templates/mdx/math-display.tsx
+++ components/templates/mdx/math-display.tsx
@@ -6,7 +6,7 @@ const MathDisplay: React.FC<JSX.IntrinsicElements['div']> = ({ className, ...res
     <div
       className={twMerge(
         clsx(
-          '-mx-4 flex max-w-[100vw] items-center overflow-x-scroll border-y border-gray-6 bg-gray-2 p-4 sm:mx-0 sm:max-w-full sm:rounded-lg sm:border-x [&>p]:my-0 [&>span]:mx-auto',
+          '-mx-4 flex max-w-[100vw] items-center overflow-x-scroll border-y border-gray-6 bg-gray-2 p-4 md:mx-0 md:max-w-full md:rounded-lg md:border-x [&>p]:my-0 [&>span]:mx-auto',
           className,
         ),
       )}
