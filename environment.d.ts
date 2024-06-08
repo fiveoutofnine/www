ca0ad21d23976b409bbab7278a51073cc960f989
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-06-08 11:33:04 +0900

    feat: ini upstash

--- environment.d.ts
+++ environment.d.ts
@@ -13,6 +13,9 @@ declare global {
       NEXT_PUBLIC_SUPABASE_URL: string;
       NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
       SUPABASE_SERVICE_KEY: string;
+      // Upstash
+      UPSTASH_URL: string;
+      UPSTASH_TOKEN: string;
       // Services
       NEXT_PUBLIC_ALCHEMY_ID: string;
       NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: string;
