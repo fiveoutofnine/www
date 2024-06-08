a4f3d8365d3d313174f753dc0fe144c28e733775
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-06-08 13:40:15 +0900

    fix: visitor logging

--- app/api/blog/views/route.tsx
+++ app/api/blog/views/route.tsx
@@ -40,19 +40,19 @@ export async function GET(request: NextRequest) {
 
     // Check if the view is a duplicate (same IP address on the post within the
     // last 24 hours).
-    const isNew = await redis.set(`blog:view_deduplicates:${id}:${hash}`, true, {
+    const seen = await redis.set(`blog:view_deduplicates:${id}:${hash}`, true, {
       nx: true,
       ex: 86_400,
     });
 
-    // Increment post visitor count if `isNew` is true.
-    visitors = await redis.hincrby('blog:visitors', id, isNew ? 1 : 0);
+    // Increment post visitor count if `seen` is false.
+    visitors = await redis.hincrby('blog:visitors', id, !seen ? 1 : 0);
   }
 
   // Update post view count.
   const views = await redis.hincrby('blog:views', id, 1);
 
-  return NextResponse.json({ id, views, visitors, ip }, { status: 200 });
+  return NextResponse.json({ id, views, visitors }, { status: 200 });
 }
 
 // -----------------------------------------------------------------------------
