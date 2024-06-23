68345700faef2f91371b45d0a3e503c77ba78834
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-06-23 15:24:28 -0400

    feat: write intro

--- app/blog/posts.ts
+++ app/blog/posts.ts
@@ -1,6 +1,11 @@
 export const POSTS: { slug: string; title: string; date: Date }[] = [
   // 2023
   {
+    slug: 'on-chain-vs-onchain',
+    title: 'On-Chain vs Onchain',
+    date: new Date('2023-07-10T19:11:55.209Z'),
+  },
+  {
     slug: 'on-chain-font',
     title: 'On-Chain Font',
     date: new Date('2023-02-22T10:54:00.000Z'),
