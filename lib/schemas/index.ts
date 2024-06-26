4304928caf4cc8b33979e71f99a9c37f4bccb57a
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-06-26 12:52:50 -0400

    feat: basically add the entire technical write-up

--- lib/schemas/index.ts
+++ lib/schemas/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 /**
- * [**Zod**](https://zod.dev/) schema for validating `{ id: number }` objects.
+ * [**Zod**](https://zod.dev) schema for validating `{ id: number }` objects.
  */
 export const idSchema = z.object({
   id: z.string().transform((x) => parseInt(x)),
