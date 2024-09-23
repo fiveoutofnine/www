b191cb0aeeb3ad5084df33885af113780bd4e05e
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-09-23 13:33:36 +0900

    feat: add replay/play/pause ability

--- app/(components)/works/on-chain-music/detail.tsx
+++ app/(components)/works/on-chain-music/detail.tsx
@@ -1,19 +1,38 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import OnChainMusicFeatureDetailModal from './modal';
 import blockie from 'ethereum-blockies-base64';
-import { ChevronFirst, Copy, Play } from 'lucide-react';
+import { ChevronFirst, Copy, Music, Pause, Play, Volume, Volume2 } from 'lucide-react';
 
 import { ON_CHAIN_SONGS } from '@/lib/constants/on-chain-music';
 
 import { ButtonGroup, IconButton, Table, toast, Tooltip } from '@/components/ui';
 
+// -----------------------------------------------------------------------------
+// Component
+// -----------------------------------------------------------------------------
+
 const OnChainMusicFeatureDetail: React.FC = () => {
-  const [song, setSong] = useState<number>();
+  const [selected, setSelected] = useState<number>();
+  const [loaded, setLoaded] = useState<number>();
+  const [audioSrc, setAudioSrc] = useState<string>();
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [replayable, setReplayable] = useState<boolean>(false);
   const [scrollIsAtTop, setScrollIsAtTop] = useState<boolean>(true);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  // Play audio when the source changes.
+  useEffect(() => {
+    if (audioRef.current && audioSrc) {
+      setPlaying(true);
+      audioRef.current.play();
+      setReplayable(true);
+    }
+  }, [audioSrc]);
 
   const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
     const target = event.target as HTMLDivElement;
@@ -58,9 +77,16 @@ const OnChainMusicFeatureDetail: React.FC = () => {
                 key={i}
                 id={`on-chain-music-feature-song-${i}`}
                 className="group text-gray-12 hover:bg-gray-4 focus:outline-none data-[state=selected]:bg-blue-5 data-[state=selected]:text-blue-12"
-                data-state={song === i ? 'selected' : undefined}
-                onClick={() => setSong(i)}
-                onFocus={() => setSong(i)}
+                data-state={selected === i ? 'selected' : undefined}
+                onClick={() => setSelected(i)}
+                onFocus={() => setSelected(i)}
+                onDoubleClick={() => {
+                  setSelected(i);
+                  setLoaded(i);
+
+                  // We set the audio source and trigger it via a `useEffect`.
+                  setAudioSrc(ON_CHAIN_SONGS[i].filePath);
+                }}
                 onKeyDown={(e) => {
                   if (e.key === 'ArrowUp' && i > 0) {
                     document.getElementById(`on-chain-music-feature-song-${i - 1}`)?.focus();
@@ -73,34 +99,40 @@ const OnChainMusicFeatureDetail: React.FC = () => {
               >
                 <Table.Cell>
                   <div className="flex items-center gap-1">
-                    <Tooltip
-                      content="Contract"
-                      side="top"
-                      align="start"
-                      triggerProps={{ className: 'rounded-sm', asChild: true }}
-                    >
-                      <button
-                        className="blocky--button group flex size-4 min-w-4 items-center justify-center overflow-hidden rounded-sm border border-gray-7 transition-colors hover:border-gray-8 group-data-[state=selected]:border-blue-7 group-data-[state=selected]:hover:border-blue-8"
-                        onClick={() => {
-                          navigator.clipboard.writeText(address);
-                          toast({
-                            intent: 'success',
-                            title: 'Copied contract address to clipboard!',
-                            description: address,
-                            hasCloseButton: true,
-                          });
-                        }}
+                    {loaded !== i ? (
+                      <Tooltip
+                        content="Contract"
+                        side="top"
+                        align="start"
+                        triggerProps={{ className: 'rounded-sm', asChild: true }}
                       >
-                        <Image
-                          className="peer transition-[filter] group-[.blocky--button]:hover:blur"
-                          src={blockie(address)}
-                          alt={`Ethereum blocky identicon for ${address}`}
-                          width={16}
-                          height={16}
-                        />
-                        <Copy className="pointer-events-none absolute size-2.5 opacity-0 transition-opacity peer-hover:opacity-100" />
-                      </button>
-                    </Tooltip>
+                        <button
+                          className="blocky--button group flex size-4 min-w-4 items-center justify-center overflow-hidden rounded-sm border border-gray-7 transition-colors animate-in fade-in zoom-in hover:border-gray-8 group-data-[state=selected]:border-blue-7 group-data-[state=selected]:hover:border-blue-8"
+                          onClick={() => {
+                            navigator.clipboard.writeText(address);
+                            toast({
+                              intent: 'success',
+                              title: 'Copied contract address to clipboard!',
+                              description: address,
+                              hasCloseButton: true,
+                            });
+                          }}
+                        >
+                          <Image
+                            className="peer transition-[filter] group-[.blocky--button]:hover:blur"
+                            src={blockie(address)}
+                            alt={`Ethereum blocky identicon for ${address}`}
+                            width={16}
+                            height={16}
+                          />
+                          <Copy className="pointer-events-none absolute size-2.5 opacity-0 transition-opacity peer-hover:opacity-100" />
+                        </button>
+                      </Tooltip>
+                    ) : (
+                      <span className="flex size-4 min-w-4 items-center justify-center text-blue-12 animate-in fade-in zoom-in">
+                        {playing ? <Volume2 className="size-4" /> : <Volume className="size-4" />}
+                      </span>
+                    )}
                     <div className="leading-4">{name}</div>
                   </div>
                 </Table.Cell>
@@ -120,22 +152,87 @@ const OnChainMusicFeatureDetail: React.FC = () => {
       </Table.Root>
 
       {/* Player */}
-      <div className="flex min-h-8 w-full items-center border-t border-gray-6 px-1.5">
+      <div className="flex min-h-8 w-full items-center gap-1.5 border-t border-gray-6 px-1.5">
         <ButtonGroup>
           <Tooltip content="Replay" side="top" align="start" triggerProps={{ asChild: true }}>
-            <IconButton size="sm">
+            <IconButton
+              size="sm"
+              disabled={!replayable}
+              onClick={() => {
+                // Reset audio player.
+                if (audioRef.current) {
+                  audioRef.current.currentTime = 0;
+                  if (audioRef.current.paused) {
+                    setReplayable(false);
+                  }
+                }
+              }}
+            >
               <ChevronFirst />
             </IconButton>
           </Tooltip>
-          <Tooltip content="Play" side="top" triggerProps={{ asChild: true }}>
-            <IconButton size="sm">
-              <Play />
+          <Tooltip
+            content={audioSrc && playing ? 'Pause' : 'Play'}
+            side="top"
+            triggerProps={{ asChild: true }}
+          >
+            <IconButton
+              size="sm"
+              onClick={
+                audioRef.current
+                  ? () => {
+                      if (audioRef.current?.paused) {
+                        audioRef.current.play();
+                        setPlaying(true);
+                        setReplayable(true);
+                      } else {
+                        audioRef.current?.pause();
+                        setPlaying(false);
+                      }
+                    }
+                  : undefined
+              }
+              disabled={!audioSrc}
+            >
+              {audioSrc && playing ? <Pause /> : <Play />}
             </IconButton>
           </Tooltip>
         </ButtonGroup>
+        <audio ref={audioRef} src={audioSrc} />
+        <div className="flex h-6 grow items-center overflow-hidden rounded border border-gray-6">
+          <div className="flex size-6 min-w-6 items-center justify-center overflow-hidden border-r border-gray-6 bg-gray-3 text-gray-11">
+            {loaded !== undefined ? (
+              <Image
+                className="animate-in fade-in zoom-in"
+                src={blockie(ON_CHAIN_SONGS[loaded].address)}
+                alt={`Ethereum blocky identicon for ${ON_CHAIN_SONGS[loaded].address}`}
+                width={24}
+                height={24}
+              />
+            ) : (
+              <Music className="size-3 animate-in fade-in zoom-in" />
+            )}
+          </div>
+          {loaded !== undefined ? (
+            <div className="flex h-6 grow flex-col items-center gap-0.5">
+              <div className="text-[10px] font-medium leading-4 text-gray-12">
+                {ON_CHAIN_SONGS[loaded].name}
+              </div>
+              <div className="h-1 min-h-1 w-full bg-gray-9" />
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
 };
+
+// -----------------------------------------------------------------------------
+// Progress meter
+// -----------------------------------------------------------------------------
+
+/* const OnChainMusicFeatureDetailProgressMeter: React.FC = () => {
+  return <div></div>;
+}; */
 
 export default OnChainMusicFeatureDetail;
--- lib/types/on-chain-music.ts
+++ lib/types/on-chain-music.ts
@@ -6,12 +6,15 @@ import type { Address } from 'viem';
  * @param chainId Chain ID the on-chain song is deployed on.
  * @param name Name of the on-chain song.
  * @param composition Source code of the on-chain song.
- * @param metadata Metadata of the on-chain song.
+ * @param filePath Relative path to the audio file of the song.
+ * @param metadata Metadata of the on-chain song in the shape of
+ * `{ sampleRate: number, samples: number }`.
  */
 export type OnChainMusicFeature = {
   address: Address;
   chainId: number;
   name: string;
+  filePath: string;
   composition: string;
   metadata: {
     sampleRate: number;
