c0abe6bb8ff01d9cd1ef5d5c15aa88a9bafc222a
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-11-05 14:13:06 +0900

    feat: add as-esport and sparta-atari-base-cover

--- lib/constants/bytebeat.ts
+++ lib/constants/bytebeat.ts
@@ -38,6 +38,18 @@ export const BYTEBEAT_SONGS: BytebeatSong[] = [
     original: 'https://dollchan.net/btb/res/204.html#424',
   },
   {
+    id: 'as-esport',
+    name: 'as esport',
+    author: {
+      name: 'blower5',
+      link: 'https://battleofthebits.com/barracks/Profile/blower5',
+    },
+    source:
+      'a=(x,y)=>[x[0]+y[0],x[1]+y[1]],p=(x)=>x.map((z)=>(t/4*1.0595**(z+((t+P)/9/2**19|0)*3)+128)%256/40),r=(x)=>a(p(q(x)),a(p([x-.1,x-.3]),p([x+.1,x+.2]))),u=(x)=>(a(r(x),S>P&S<P*4|S>P*5?r(x+11.8):Z)),s=(x)=>u(x[T=(S>>45)%32]??-9e9),w=(x)=>p(q(x)).map(x=>(x*4&16)*1.8),q=(x)=>[x,x],Z=q(0),P=8<<16,S=t%(P*9),this.v??=0,R=(w,i,s)=>(v++||(x=Array(8<<10).fill(0)),y=w[1]%256+x.shift(),x.push(y*s),[y*i+w[0]*.7,y*i+w[1]*.5]),A=[38,,,38,,,38,41,,,41,,,41,,,36,,,36,,,36,38,,,38,,,38],B=[31,,,31,,,31,34,,,34,,,34,,,29,,,29,,,29,31,,,31,,,31,,29],C=[46,,,46,,,46,46,,,46,,,48,,,45,,,45,,,45,46,,,46,,,46,,53],D=[7,10,5,7],E=[43,38,50,46,,46,45,41],F=[,38,41,38,36,41,38,,],G=[43,,45,46,48,50,43],E=E.concat(F,E,G),H=S<P*5?[8,,3,]:[8,5,4,3],a(a(R(a(a(a(s(A),s(B)),a(s(B.map(x=>x-12)),s(C))),S>P*4?q(w(E[T%32])[0]*(H[(4*t>>45)%4]??0)*.1):Z),.5,-.5),S>P*6|S<P*4?T&1?a(w(D[T>>3]),w(D[T>>3]-11.9)):Z:Z),a(S>P*2&S<P*4|S>P*6?~T&1?q((1e2**(t*2%(8<<12))**.05)&32):Z:Z,q(random()*(256-(t>>4)%256)*[4,2,8,3][(t>>44)%4]/75)))',
+    sampleRate: 48000,
+    original: 'https://battleofthebits.com/arena/Entry/as+esport/61968',
+  },
+  {
     id: '347-bytebeat-inc',
     name: '347 BYTEBEAT INC.',
     author: {
@@ -48,6 +60,18 @@ export const BYTEBEAT_SONGS: BytebeatSong[] = [
       '128+24*sin(t/24*(t>>10&42)+sin(t/64*(t/1>>15)&21))+sin(t/12+4*sin(t/24))*(t>>11&1)*16+sin(t/24+4*sin(t/48))*(t>>10&1)*16+56*(3E3/(y=4*t&16383)&1)+56*(300/(y=32*t&28600)&1)+12*sin(t/16*(t>>10&42)+sin(t/64*(t/1>>7)&8))',
     sampleRate: 11025,
     original: 'https://battleofthebits.com/arena/Entry/347+BEATSTEP+INC./20403',
+  },
+  {
+    id: 'sparta-atari-base-cover',
+    name: '"Sparta Atari Base" cover',
+    author: {
+      name: 'ANoUserXD',
+    },
+    source:
+      'T=1.15*t,k=T>>11,R=random,c=([a=4E4*R()/(T&4095),a,0,0,a,a,0,0,a,0,b=4E4*R()/(T&2047),0,b,b,4E4*R()/(T&1023),4E4*R()/(T&511)][k&15]&255)%256/2,d=(4E4*R()/(T&[4095,4095,4095,2047][T>>12&3])&255)%256/2,e=t*(0xAFEDC320>>t/2.65*2**([-3,4,-3,4,-2,5,-2,5,-5,2,-5,2,-2,5,-2,5][k&15]/12)&1)&&64,f=t*(0xAFEDC320>>t/2.65*2**([-15,-14,-17,-14][T>>13&3]/12)&1)&&64,g=4*t*[2.5,-2.5,2.6,-2.6,2.2,-2.2,2.6,-2.6][T>>12&7]&64,h=t*[1.25,,1.25,,1.3,,1.3,,1.1,,1.1,,1.3,,1.3][k&15]&64,i=(t*2**([16,,23,,29,,24,,14,,21,14,29,,24][k&15]/12)&63)+16&64,j=2*t*[1.9,1.9,3.8,,2,2,4,,1.7,1.7,3.4,2.5,2,4,2,4][k&15]&64,k=(4E4*R()/(T&[0,0,0,2047][T>>13&3])&255)%256/2,l=8*t*2**([4,4,12,12,16,16,,,5,5,17,5,5,17,17,17,2,2,14,14,,,,,17,17,5,5,2,2,5,5][T>>10&31]/12)&64,m=(8*t*2**([4,4,,4,4,,4,,5,5,,5,5,,5,,2,2,,2,2,,2,,5,5,,5,5,,5][T>>10&31]/12)&64)*(~T>>2&255)>>8,min(255,[c,d+e+f,d+e+f,g+h,g+h,g+h+d,d+e+f,d+e+f,d+e+f+i,d+e+f+i,j+h+k,j+h+k,d+e+f,d+e+f,d+e+f+l,d+e+f+l,m,m,m+i,m+i,d+e+f,d+e+f,d+e+f+i,d+e+f+i,j+h,j+h,j+h,j+h,j,j,d+e+f+m,d+e+f+m,d+e+f+i,d+e+f+i,d+e+f+l,d+e+f+l][T>>15])',
+    sampleRate: 8000,
+    original:
+      'https://www.reddit.com/r/bytebeat/comments/155a8c0/cover_of_sparta_atari_base_completed',
   },
 ];
 
