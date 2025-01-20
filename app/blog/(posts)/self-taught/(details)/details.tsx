58461872f5ffa1cc9338960dae3206c13f58b196
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-01-20 15:06:40 -0500

    feat: add fosbury

--- app/blog/(posts)/self-taught/(details)/details.tsx
+++ app/blog/(posts)/self-taught/(details)/details.tsx
@@ -1,2 +1,4 @@
+export { default as Fosbury1 } from './fosbury-1.mdx';
+export { default as Fosbury2 } from './fosbury-2.mdx';
 export { default as Ramanujan1 } from './ramanujan-1.mdx';
 export { default as Ramanujan2 } from './ramanujan-2.mdx';
