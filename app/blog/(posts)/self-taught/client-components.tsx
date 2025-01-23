0998a46800f52466bf79d1ecf37bb0eec9af3ace
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-01-22 23:37:28 -0500

    fix: delete aria label to remove inconsistency

--- app/blog/(posts)/self-taught/client-components.tsx
+++ app/blog/(posts)/self-taught/client-components.tsx
@@ -199,7 +199,6 @@ export const DetailModal: React.FC<DetailModalProps> = ({
       <Drawer.Trigger asChild>
         <span
           className="h-5 cursor-pointer rounded-sm text-gray-11 underline decoration-dotted transition-colors hover:text-gray-12 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-9"
-          aria-label="View detail"
           tabIndex={0}
           role="button"
           onClick={onClick}
