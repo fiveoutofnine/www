746a53638c97ee936baea89ff1efee974490b6fc
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-04-04 15:25:00 -0400

    feat: ini api route

---
+++ lib/utils/getRandomMp4Url.ts
@@ -0,0 +1,30 @@
+// -----------------------------------------------------------------------------
+// Constants
+// -----------------------------------------------------------------------------
+
+const BASE_URL = 'https://assets.fiveoutofnine.com/mp4/';
+const NUMBER_OF_VIDEOS = 3;
+
+// -----------------------------------------------------------------------------
+// Function
+// -----------------------------------------------------------------------------
+
+/**
+ * Get a random MP4 URL from the list of curated videos.
+ * @param exclude Optional index to exclude from the random selection. Only used
+ * when there's more than 1 MP4 video (i.e. `NUMBER_OF_VIDEOS > 1`).
+ * @returns The URL of a random MP4 video.
+ */
+const getRandomMp4Url = (exclude?: number) => {
+  let seed = Math.floor(Math.random() * NUMBER_OF_VIDEOS);
+
+  // Increase the seed by 1 to avoid the exclusion if there's only 1 MP4 video.
+  if (seed === exclude && NUMBER_OF_VIDEOS > 1) seed = (seed + 1) % NUMBER_OF_VIDEOS;
+
+  return {
+    url: `${BASE_URL}${seed.toString(16).padStart(8, '0')}.mp4`,
+    index: seed,
+  };
+};
+
+export default getRandomMp4Url;
--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -2,4 +2,5 @@ export { default as formatValueToPrecision } from './formatValueToPrecision';
 export { default as getBlogPageMetadata } from './getBlogPageMetadata';
 export { default as getColormapValue } from './getColormapValue';
 export { default as getDesignPageMetadata } from './getDesignPageMetadata';
+export { default as getRandomMp4Url } from './getRandomMp4Url';
 export { default as validateQuery } from './validateQuery';
