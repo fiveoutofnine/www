2399d0491d5aac4d19069528b6b3a8a2749e82d9
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-04-05 02:33:35 -0400

    feat: add show/hide control

--- app/(components)/works/mp4/detail.tsx
+++ app/(components)/works/mp4/detail.tsx
@@ -2,7 +2,16 @@
 
 import { Fragment, useEffect, useRef, useState } from 'react';
 
-import { PanelLeftClose, Pause, Play, SkipForward, Volume2, VolumeOff } from 'lucide-react';
+import { motion } from 'framer-motion';
+import {
+  PanelLeftClose,
+  PanelLeftOpen,
+  Pause,
+  Play,
+  SkipForward,
+  Volume2,
+  VolumeOff,
+} from 'lucide-react';
 
 import { getRandomMp4Url } from '@/lib/utils';
 
@@ -14,7 +23,7 @@ const Mp4FeatureDetail: React.FC = () => {
   const [playing, setPlaying] = useState<boolean>(false);
   const [muted, setMuted] = useState<boolean>(false);
   const [showOverlay, setShowOverlay] = useState<boolean>(false);
-
+  const [showControls, setShowControls] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => setMounted(true), []);
@@ -39,8 +48,12 @@ const Mp4FeatureDetail: React.FC = () => {
   };
 
   return (
-    <div className="flex h-full w-full flex-col">
-      <div className="relative flex max-h-[8.875rem] w-full grow items-center justify-center overflow-hidden bg-black">
+    <div className="relative flex h-full w-full flex-col">
+      <motion.div
+        className="relative flex w-full grow items-center justify-center overflow-hidden bg-black"
+        animate={{ maxHeight: showControls ? 142 : 182 }}
+        transition={{ type: 'spring', duration: 0.25 }}
+      >
         {mp4 ? (
           <Fragment>
             <video
@@ -72,8 +85,17 @@ const Mp4FeatureDetail: React.FC = () => {
         ) : (
           <div className="flex h-full w-full items-center justify-center bg-gray-3">Loading...</div>
         )}
-      </div>
-      <div className="flex h-10 w-full items-center justify-between border-t border-gray-6 px-2">
+      </motion.div>
+      <motion.div
+        className="absolute bottom-0 left-0 flex w-full items-center justify-between border-gray-6 px-2"
+        animate={{
+          height: showControls ? 40 : 0,
+          borderTopWidth: showControls ? 1 : 0,
+          opacity: showControls ? 1 : 0,
+          pointerEvents: showControls ? 'auto' : 'none',
+        }}
+        transition={{ type: 'spring', duration: 0.25 }}
+      >
         <div className="flex gap-1">
           <ButtonGroup>
             <Tooltip
@@ -125,11 +147,36 @@ const Mp4FeatureDetail: React.FC = () => {
           </Tooltip>
         </div>
         <Tooltip content="Hide controls" side="top" align="end" triggerProps={{ asChild: true }}>
-          <IconButton size="sm" aria-label="Hide controls">
+          <IconButton
+            size="sm"
+            aria-label="Hide controls"
+            onClick={() => setShowControls(!showControls)}
+          >
             <PanelLeftClose className="-rotate-90" />
           </IconButton>
         </Tooltip>
-      </div>
+      </motion.div>
+      {!showControls ? (
+        <motion.div
+          className="absolute bottom-2 right-2 animate-in fade-in"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ type: 'spring', delay: 0.1, duration: 0.25 }}
+        >
+          <Tooltip content="Show controls" side="left" triggerProps={{ asChild: true }}>
+            <IconButton
+              className="backdrop-blur"
+              size="sm"
+              variant="outline"
+              aria-label="Show controls"
+              onClick={() => setShowControls(!showControls)}
+            >
+              <PanelLeftOpen className="-rotate-90" />
+            </IconButton>
+          </Tooltip>
+        </motion.div>
+      ) : null}
     </div>
   );
 };
