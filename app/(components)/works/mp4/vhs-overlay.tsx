2779a0c7c045d8e27a5f0bb819f244fa1829ac5f
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-04-05 16:59:39 -0400

    feat: add labels

--- app/(components)/works/mp4/vhs-overlay.tsx
+++ app/(components)/works/mp4/vhs-overlay.tsx
@@ -1,11 +1,37 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import { motion } from 'framer-motion';
 import { ChevronDown, Play } from 'lucide-react';
 
 import { NUMBER_OF_VIDEOS } from '@/lib/utils/getRandomMp4Url';
 
 const VHSOverlay: React.FC = () => {
+  const [time, setTime] = useState<string>('');
+
+  // Update the time every second.
+  useEffect(() => {
+    const updateTime = () => {
+      const options: Intl.DateTimeFormatOptions = {
+        timeZone: 'America/New_York',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      };
+
+      const estTime = new Intl.DateTimeFormat('en-US', options).format(new Date());
+      setTime(estTime);
+    };
+
+    updateTime();
+
+    const intervalId = setInterval(updateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div
       className="pointer-events-none absolute inset-0 z-10 flex items-center justify-center"
@@ -48,7 +74,10 @@ const VHSOverlay: React.FC = () => {
         {/* Top-right label. */}
         <span className="absolute right-2 top-2">--:--</span>
         {/* Bottom-left label. */}
-        <div className="absolute bottom-2 left-2">{NUMBER_OF_VIDEOS} videos</div>
+        <div className="absolute bottom-2 left-2 flex flex-col items-start gap-0.5">
+          <span>The Internet</span>
+          <span>{NUMBER_OF_VIDEOS} videos</span>
+        </div>
         {/* Bottom-center label. */}
         <div className="absolute bottom-2 flex animate-pulse items-center gap-1">
           <span className="flex size-3 items-center justify-center">
@@ -60,7 +89,10 @@ const VHSOverlay: React.FC = () => {
           </span>
         </div>
         {/* Bottom-right label. */}
-        <div className="absolute bottom-2 right-2">VHS</div>
+        <div className="absolute bottom-2 right-2 flex flex-col items-end gap-0.5">
+          <span>New York, USA</span>
+          <span>{time}</span>
+        </div>
         {/* Center error label. */}
         <div className="flex flex-col items-start text-xl leading-7 mix-blend-screen">
           <span>No cassette</span>
