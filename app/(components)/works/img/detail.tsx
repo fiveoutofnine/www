afd7f151a2fc026dbc82fe37d80bd46f7dc633a6
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-04-07 13:45:40 -0400

    feat: ini image layout

--- app/(components)/works/img/detail.tsx
+++ app/(components)/works/img/detail.tsx
@@ -1,7 +1,21 @@
 'use client';
 
+import { useState } from 'react';
+
+import { getRandomImgUrl } from '@/lib/utils';
+
 const ImgFeatureDetail: React.FC = () => {
-  return <div>ImgFeatureDetail</div>;
+  const [image, setImage] = useState<ReturnType<typeof getRandomImgUrl>>();
+  const [nextImage, setNextImage] = useState<ReturnType<typeof getRandomImgUrl>>();
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  return (
+    <div className="flex h-[11.375rem] w-full overflow-hidden bg-black">
+      <div className="h-full w-8 bg-red-9" />
+      <div className="h-full grow bg-black" />
+      <div className="h-full w-8 bg-green-9" />
+    </div>
+  );
 };
 
 export default ImgFeatureDetail;
