183e1ed10ddbb5975ce2c9a5e75707d53fa5582a
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-04-10 04:08:01 -0400

    fix: file extension typo

--- lib/utils/getRandomMp4Url.ts
+++ lib/utils/getRandomMp4Url.ts
@@ -23,7 +23,7 @@ const getRandomMp4Url = (exclude?: number) => {
   if (seed === exclude && NUMBER_OF_VIDEOS > 1) seed = (seed + offset) % NUMBER_OF_VIDEOS;
 
   return {
-    url: `${BASE_URL}${Math.min(seed, 999_999_999).toString(10).padStart(9, '0')}.jpg`,
+    url: `${BASE_URL}${Math.min(seed, 999_999_999).toString(10).padStart(9, '0')}.mp4`,
     index: seed,
   };
 };
